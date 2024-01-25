f295bead19bb1d9cca74a85db71beca1c5b524dd
MariaDim03 <marydimi03@gmail.com>
2024-01-25 14:01:48 +0200

    +5a

--- Code/Queries/query3.sql
+++ Code/Queries/query3.sql
@@ -1,6 +1,7 @@
 WITH Vessels_over_30 AS (
 	SELECT DISTINCT type, vessel_id FROM Positions AS p 
 	INNER JOIN Vessels AS v ON p.vessel_id=v.id
-	WHERE speed>30)
+	WHERE speed>30
+	)
 SELECT count(*), type FROM Vessels_over_30
 GROUP BY type
---
+++ Code/Queries/query5a.sql
@@ -0,0 +1,4 @@
+SELECT DISTINCT vessel_id FROM Positions AS p
+INNER JOIN Vessels AS v ON p.vessel_id=v.id
+INNER JOIN VesselTypes AS vt ON v.type=vt.code
+WHERE description LIKE 'Cargo%' AND speed = '0' AND t >= '2019-08-15 00:00:00' AND t <= '2019-08-18 23:59:59'
